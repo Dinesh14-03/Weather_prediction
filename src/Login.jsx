611cca9752b06d1baf725ae4ec0113103d116103
Dinesh <dineshponnusami2020@gmail.com>
2026-02-25 12:08:17 +0530

    first

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,10 @@
 import './App.css';
 function Login(){
-
+   
     return(
         <>
           <h1>Plese select the weather status</h1>
-          <button style={{backgroundColor}}>Click here</button>
-          
+          <button style={{backgroundColor:'blue'}} onClick={weather()}>Click here</button>
         </>
 
     )
