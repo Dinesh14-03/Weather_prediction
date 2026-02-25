7b5bcfda3dd6b76ca16dcc875e26f8b3f9625732
Dinesh <dineshponnusami2020@gmail.com>
2026-02-25 12:20:38 +0530

    first

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,36 @@
 import './App.css';
-function Login(){
-   
-    return(
-        <>
-          <h1>Plese select the weather status</h1>
-          <button style={{backgroundColor:'blue'}} onClick={weather()}>Click here</button>
-        </>
+import data from './response.json';
 
-    )
+function Login() {
+
+    const login = () => {
+        const items = [];
+
+        for (let key in data) {
+            items.push(
+                <h2 key={key}>
+                    {key} : {data[key]}
+                </h2>
+            );
+        }
+
+        return items;
+    };
+
+    return (
+        <>
+            <h1>Please select the weather status</h1>
+
+            <button
+                style={{ backgroundColor: 'blue', color: 'white' }}
+                onClick={() => alert('Scroll down to see data')}
+            >
+                Click here
+            </button>
+
+            {login()}
+        </>
+    );
 }
 
-export default Login
+export default Login;
